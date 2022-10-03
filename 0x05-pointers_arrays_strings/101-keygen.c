bd5525d9335e791089000dd13b611f9573d7affb
fekins92 <fekinsbest@gmail.com>
2022-10-03 00:16:00 -0700

    0. It aint what they call you, its what you answer to

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for the
+ * 	  program 101_crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char password[84]:
